c93a890b6ba193d79a84d4a53cc1b736b410b607
It's Me <itsme@sorry.com>
2023-07-15 13:47:41 +0600

    Students UI created

---
+++ src/components/Students.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { useStudents } from '../stores/counter';
+const students = useStudents();
+</script>
+
+<template>
+    <section>
+        <h2>Welcome, Nadia!</h2>
+        <span>My Status <span class="T-blue">View All</span></span>
+    </section>
+
+    <div class="students flex">
+        <div v-for="student in students.students" class="student flex flex-y-center" >
+            <div class="profile flex flex-center">
+                <img width="100" height="100" class="no-select no-drag" :src="`./src/assets/images/${student.profile}`" :alt="student.name" :srcset="`./src/assets/images/${student.profile}`">
+            </div>
+            <div class="info">
+                <h5>{{ student.name }}</h5>
+                <span>{{ student.status }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+.students {
+    flex-wrap: wrap;
+}
+
+.student {
+    width: 100%;
+    margin: 10px;
+    max-width: 350px;
+    padding: 25px 15px;
+    border-radius: 12px;
+    background: #ffffff;
+    align-items: flex-start;
+}
+
+.student span {
+    color: var(--gray);
+}
+
+.profile {
+    width: 40px;
+    height: 40px;
+    max-width: 40px;
+    min-width: 40px;
+    max-height: 40px;
+    min-height: 40px;
+    aspect-ratio: 1/1;
+    margin-right: 15px;
+}
+
+.profile img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+</style>
